00d6501beb3ace97a58fe604f5fe59521ac31175
wendf <fengGe_wen@163.com>
2016-11-23 17:38:27 +0800

    navigatorIos

--- Class/Root/root.js
+++ Class/Root/root.js
@@ -8,45 +8,54 @@ import React, { Component } from 'react';
 import {
   AppRegistry,
   StyleSheet,
+  NavigatorIOS,
+  TouchableOpacity,
   Text,
   View,
   Image
 } from 'react-native';
 
+import view1 from '../components/view1';
+
 class root extends Component{
   render(){
     return(
-      <View style={styles.flexContainer} >
-        <Text style={styles.text} >
-          1
-        </Text>
-        <Text style={[styles.text,styles.full]} >
-          2
-        </Text>
-        <Text style={[styles.text,{flex:1}]} >
-          3
-        </Text>
-      </View>
+      <NavigatorIOS ref="nav" style={styles.container} translucent={false} initialRoute={{
+        component: HomeScene,
+        title: '导航',
+      }}/>
     );
   }
 }
+class HomeScene extends Component{
+
+    render() {
+        return (
+            <View style={[styles.scene, {backgroundColor: '#DAF6FF'}]}>
+                <TouchableOpacity onPress={this.onPress.bind(this)}>
+                    <Text>欢迎使用NavigatorIOS Demo. 点击导航!</Text>
+                </TouchableOpacity>
+            </View>
+        );
+    }
+    onPress() {
+        this.props.navigator.push({
+            title: '从TouchableOpacity导航到该页面',
+            component: view1
+        });
+    }
+}
+
 const styles = StyleSheet.create({
-  text: {
-    fontSize: 30,
-    color: '#ffffff',
-    backgroundColor: '#000000',
-    textAlign: 'center',
-    width: 40,
-    height: 40,
-    margin: 20
-  },
-  flexContainer: {
-    flexDirection: 'row',
-  },
-  full:
-  {
-    flex: 1
-  }
+  container: {
+        flex: 1
+    },
+    scene: {
+        padding: 10,
+        paddingTop: 74,
+        flex: 1
+    }
+
 });
 // export default root;
 module.exports = root;
